686547300f3b00ebfa2028854163d95c3c065185
hrafael <enriquergs13@gmail.com>
2024-05-03 22:57:06 -0300

    finalizado

--- CP2-Web/src/pages/Projetos/Projetos.jsx
+++ CP2-Web/src/pages/Projetos/Projetos.jsx
@@ -34,38 +34,38 @@ function Projeto() {
             id: 2,
             nome: 'Bits-0f-C0de',
             descricaoCurta: 'Minha página de blog pessoal foi construída com Next.js e Tailwind Css, que pega o conteúdo dos arquivos makdown e o renderiza usando Next.js. Suporta modo escuro e é fácil de escrever blogs usando markdown.',
-            link: 'https://github.com/soumyajit4419/Bits-0f-C0de'
+            link: 'https://github.com/seu-usuario/seu-projeto'
         },
         {
             id: 3,
             nome: 'Editor.io',
             descricaoCurta: 'Código online e editor de markdown construído com react.js. Editor online que suporta código HTML, CSS e JS com visualização instantânea do site. Editor de markdown on-line para criar arquivo README que suporta GFM, tags HTML personalizadas com barra de ferramentas e visualização instantânea. Ambos os editores suportam salvamento automático de trabalho usando armazenamento local.',
-            link: 'https://github.com/soumyajit4419/Editor.io'
+            link: 'https://github.com/seu-usuario/seu-projeto'
         },
         {
             id: 4,
             nome: 'Planta IA',
             descricaoCurta: 'Usei o conjunto de dados de doenças de plantas do Kaggle e treinei um modelo de classificador de imagens usando a estrutura PyTorch usando CNN e Transfer Learning com 38 classes de várias folhas de plantas. O modelo foi capaz de detectar com sucesso folhas doentes e saudáveis ​​de 14 plantas únicas. Consegui atingir uma precisão de 98% usando o modelo pré-treinado Resnet34.',
-            link: 'https://github.com/soumyajit4419/Plant_AI'
+            link: 'https://github.com/seu-usuario/seu-projeto'
         },
         {
             id: 5,
             nome: 'IA para o bem social',
             descricaoCurta: 'Utilizar Processamento de Linguagem Natural para detectar postagens relacionadas ao suicídio e ideação suicida de usuários no ciberespaço e assim ajudar na prevenção do suicídio.',
-            link: 'https://github.com/soumyajit4419/AI_For_Social_Good'
+            link: 'https://github.com/seu-usuario/seu-projeto'
         },
         {
             id: 6,
             nome: 'Reconhecimento facial e detecção de emoções',
             descricaoCurta: 'reinei um classificador CNN usando conjunto de dados FER-2013 com Keras e tensorflow apoiado. O classificador previu com sucesso os vários tipos de emoções humanas. E a maior precisão obtida com o modelo foi de 60,1%. Em seguida, usei o Open-CV para detectar o rosto em uma imagem e depois passar o rosto para o classificador para prever a emoção de uma pessoa.',
-            link: 'https://github.com/soumyajit4419/Face_And_Emotion_Detection'
+            link: 'https://github.com/seu-usuario/seu-projeto'
         },
     ];
 
     return (
         <>
             <Header />
-            <h1 className="text-8xl flex justify-center items-center p-10">Projetos</h1>
+            <h1 className="text-8xl p-10  text-gray-400 flex flex-col items-start gap-10 pr-10 pl-10">Projetos</h1>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
                 {projetos.map(projeto => (
                     <CardProjeto key={projeto.id} projeto={projeto} />
