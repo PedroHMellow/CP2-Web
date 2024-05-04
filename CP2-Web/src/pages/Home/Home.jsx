3bf1ea48bdb7330dd766b78062df1ebccdc1f43f
hrafael <enriquergs13@gmail.com>
2024-05-03 21:24:34 -0300

    subindo parte de projetos

--- CP2-Web/src/pages/Home/Home.jsx
+++ CP2-Web/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
-
 function Home() {
   return (
     <>
@@ -15,11 +14,20 @@ function Home() {
         />
       </figure>
       
-    {/* Colocar Apretasão de equipe */}
+      <h1 className="text-white"> DEIXE-ME NOS APRESENTAR </h1>
+      <p className="text-white">SOMOS DEVS EM FORMAÇÃO, SOMOS DO GRUPO 4 DO 1ESPB  (ENGENHARIA DE SOFTWARE), TEMOS MUITO INTERESSE POR TECNOLOGIAS NOVAS E SEMPRE PROCURAMOS NOS APROFUNDAR CADA VEZ MAIS NOS ASSUNTOS QUE ESTAMOS APRENDENDO</p>  
+      <br />
+      <p className="text-white">PARA ENTRAR EM CONTATO COM NOS BASTA UTILIZAR OS EMAILS A BAIXO</p>
+      <br />
+      <p className="text-white">Pedro Melo - rm554223@fiap.com.br</p>
+      <br />
+      <p className="text-white">Henrique Rafael - rm553945@fiap.com.br</p>
+      <br />
+      <p className="text-white">Gustavo Gouvea - rm553842@fiap.com.br</p>
 
       <Footer />
     </>
   );
 }
 
-export default Home 
+export default Home;
--- CP2-Web/src/pages/Projetos/Projetos.jsx
+++ CP2-Web/src/pages/Projetos/Projetos.jsx
@@ -1,13 +1,79 @@
-import Header from "../../components/Header/Header"
-import Footer from "../../components/Footer/Footer"
-function Projeto () {
+import React from 'react';
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+function CardProjeto({ projeto }) {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const toggleDetails = () => setIsOpen(!isOpen);
+
+    return (
+        <div className="m-4 p-4 border rounded shadow-lg hover:bg-gray-100 cursor-pointer" onClick={toggleDetails}>
+            <h3 className="text-lg font-bold">{projeto.nome}</h3>
+            <p>{projeto.descricaoCurta}</p>
+            {isOpen && (
+                <div>
+                    <p>{projeto.descricaoCompleta}</p>
+                    {projeto.imagem && <img src={projeto.imagem} alt={projeto.nome} className="w-full mt-4" />}
+                </div>
+            )}
+        </div>
+    );
+}
+
+function Projeto() {
+    const projetos = [
+        {
+            id: 1,
+            nome: 'Chatificar',
+            descricaoCurta: 'Sala de bate-papo pessoal ou espaço de trabalho para compartilhar recursos e hangout com amigos criados com react.js, Material-UI e Firebase. Possui recursos que permitem ao usuário mensagens em tempo real, compartilhamento de imagens e também suporta reações nas mensagens..',
+            imagem: 'https://github.com/soumyajit4419/Chatify'
+        },
+        {
+            id: 2,
+            nome: 'Bits-0f-C0de',
+            descricaoCurta: 'Minha página de blog pessoal foi construída com Next.js e Tailwind Css, que pega o conteúdo dos arquivos makdown e o renderiza usando Next.js. Suporta modo escuro e é fácil de escrever blogs usando markdown.',
+            imagem: 'url-da-imagem.jpg'
+        },
+        {
+            id: 3,
+            nome: 'Editor.io',
+            descricaoCurta: 'Código online e editor de markdown construído com react.js. Editor online que suporta código HTML, CSS e JS com visualização instantânea do site. Editor de markdown on-line para criar arquivo README que suporta GFM, tags HTML personalizadas com barra de ferramentas e visualização instantânea. Ambos os editores suportam salvamento automático de trabalho usando armazenamento local.',
+            imagem: 'url-da-imagem.jpg'
+        },
+        {
+            id: 4,
+            nome: 'Projeto 4',
+            descricaoCurta: 'Breve descrição do Projeto 4.',
+            imagem: 'url-da-imagem.jpg'
+        },
+        {
+            id: 5,
+            nome: 'Projeto 5',
+            descricaoCurta: 'Breve descrição do Projeto 5.',
+            imagem: 'url-da-imagem.jpg'
+        },
+        {
+            id: 6,
+            nome: 'Projeto 6',
+            descricaoCurta: 'Breve descrição do Projeto 6.',
+            imagem: 'url-da-imagem.jpg'
+        },
+       
+    ];
+
     return (
         <>
             <Header />
-            
+            <h1 className="text-8xl flex justify-center items-center p-10">Projetos</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
+                {projetos.map(projeto => (
+                    <CardProjeto key={projeto.id} projeto={projeto} />
+                ))}
+            </div>
             <Footer />
         </>
-    )
+    );
 }
 
-export default Projeto
+export default Projeto;
